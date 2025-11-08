6ca412cb7d00b8c9680b9707e3909ee931f7b9d5
fundav <anyuse51234@gmail.com>
2025-11-08 17:58:24 -0600

    small change

--- backend/utils.py
+++ backend/utils.py
@@ -1,3 +1,4 @@
+from haversine import haversine
 import hashlib
 import json
 
@@ -9,5 +10,14 @@ def calculate_hash(data_to_hash, previous_hash):
     return hashlib.sha256(block_string).hexdigest()
 
 
-def haversine_audit_logic():
-    pass
+def haversine_audit_logic(data):
+    SPEED = 300 #Suspicion (km/hr)
+    location1 = (45.7597, 4.8422)
+    time1 = 1
+    for i in range(len(data)):
+        data[i]
+        location2 = (48.8567, 2.3508)
+        time2 = 0
+        if (time2 - time1) <= 0:
+            return False
+        speed = haversine(location1, location2) / time2 - time1
