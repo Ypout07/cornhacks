bbe93e42339a6da00d43cd3e1ebc31a088458611
fundav <anyuse51234@gmail.com>
2025-11-08 14:27:55 -0600

    id work

--- backend/app.py
+++ backend/app.py
@@ -9,23 +9,13 @@ temp = {}
 def home():
     return jsonify({"message" : "LMAO OK"})
 
-"""
-{
-        "farm_name": "BananaCorp",
-        "harvest_date": "XXXX-XX-XX",
-        "quantity_kg": 6767, 
-        "latitude": 10.45,
-        "longitude": 67.67,
-        "grade": "spectacular",
-        "produce": "bananas"
-        }
-"""
 @app.route("/api/batch", methods=["POST"])
 def init_batch():
     batch_data = request.get_json()
     id = batch_data["farm_name"][0] + batch_data["harvest_date"][0] + str(batch_data["quantity_kg"])[0]
     id += str(batch_data["latitude"])[0] + str(batch_data["longitude"])[0] + batch_data["grade"][0] + batch_data["produce"][0]
     id += str(random.randint(0,9)) + str(random.randint(0,9)) + str(random.randint(0,9)) + str(random.randint(0,9))
+    temp[id] = batch_data
     return jsonify({"message" : "Batch has been initialized",
             "batch_uuid" : id})
 
@@ -60,9 +50,9 @@ Received:
 …
 } ]
 """
-@app.route("/api/batch/<uuid>", methods=["POST"])
-def get_data():
-    return jsonify(temp)
+@app.route("/api/batch/<id>", methods=["POST"])
+def get_data(id):
+    return jsonify(temp[id])
 
 if __name__ == '__main__':
     app.run()
